5c25ce53d6f974679fe9869cd2b04597cbac06de
YousefSaleh1 <ys9882111@gmail.com>
2024-06-10 00:48:30 +0300

    Update AdvatageController

--- app/Http/Controllers/AdvatageController.php
+++ app/Http/Controllers/AdvatageController.php
@@ -2,51 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Advatage;
+use App\Http\Requests\Advantage\UpdateAdvantageRequest;
+use App\Http\Resources\AdvantageResource;
 use Illuminate\Http\Request;;
 use App\Http\Resources\AdvatageResource;
 use App\Http\Traits\ApiResponseTrait;
+use App\Models\Advantage;
 use Illuminate\Support\Facades\Log;
 
 class AdvatageController extends Controller
 {
     use ApiResponseTrait;
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $advatages = Advatage::all();
-        $data = AdvatageResource::collection($advatages);
-        return $this->customeResponse($data, 'Done!', 200);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        try {
-            //code...
-        } catch (\Throwable $th) {
-            Log::error($th);
-            return $this->customeResponse(null, 'Failed To Create', 500);
-        }
-    }
 
     /**
      * Display the specified resource.
      */
-    public function show(Advatage $advatage)
+    public function show(Advantage $advatage)
     {
-        $data = new AdvatageResource($advatage);
+        $data = new AdvantageResource($advatage);
         return $this->customeResponse($data, 'Done!', 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Advatage $advatage)
+    public function update(UpdateAdvantageRequest $request, Advantage $advatage)
     {
         try {
             //code...
@@ -56,12 +36,4 @@ class AdvatageController extends Controller
         }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Advatage $advatage)
-    {
-        $advatage->delete();
-        return response()->json(['message' => '{{ Model }} Deleted'], 200);
-    }
 }

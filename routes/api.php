a0e1de2a73a98937b5c40a86448903fc50f90030
rashait95 <rashabaakhras@gmial.com>
2024-06-18 02:05:43 +0300

    edit contact messages routes and remove  auth middlware

--- routes/api.php
+++ routes/api.php
@@ -37,11 +37,11 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('api');
 //////////////////////Contact Messages Routes ////////////////////////
 Route::post('/addContactMessage', [ContactMessageController::class, 'store']);
 
-Route::middleware(['auth:api'])->group(function () {
+//Route::middleware(['auth:api'])->group(function () {
 Route::get('/allContactMessages',[ContactMessageController::class,'index']);
 Route::get('/showContactMessage/{contactMessage}',[ContactMessageController::class,'show']);
 Route::delete('/deleteContactMessage/{contactMessage}',[ContactMessageController::class,'destroy']);
-});
+//});
 
 /////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////// Hero Requests ////////////////////////////////////
